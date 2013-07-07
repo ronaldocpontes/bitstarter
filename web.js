0896af3977c2a1c822efd9149e94dfe6118e2fd7
Ronaldo Pontes <ronaldocpontes@yahoo.com.br>
2013-07-07 17:52:58 +0100

    Updated web.js

--- web.js
+++ web.js
@@ -3,10 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+	var index = fs.readFileSync('index.html', {encoding:'utf8'})
+	response.send(index);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
