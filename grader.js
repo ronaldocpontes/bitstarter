327665630ad157dfae75e02abf542dcf997ccee9
Ronaldo Pontes <ronaldocpontes@yahoo.com.br>
2013-07-07 19:57:41 +0100

    grader

---
+++ grader.js
@@ -0,0 +1,93 @@
+#!/usr/bin/env node
+/*
+ * Automatically grade files for the presence of specified HTML tags/attributes.
+ * Uses commander.js and cheerio. Teaches command line application development
+ * and basic DOM parsing.
+ * 
+ * References: + cheerio - https://github.com/MatthewMueller/cheerio -
+ * http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/ -
+ * http://maxogden.com/scraping-with-node.html + commander.js -
+ * https://github.com/visionmedia/commander.js -
+ * http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy +
+ * JSON - http://en.wikipedia.org/wiki/JSON -
+ * https://developer.mozilla.org/en-US/docs/JSON -
+ * https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+ */
+
+var sys = require('util');
+var fs = require('fs');
+var rest = require('restler');
+
+var program = require('commander');
+var cheerio = require('cheerio');
+var HTMLFILE_DEFAULT = "index.html";
+var CHECKSFILE_DEFAULT = "checks.json";
+
+var assertFileExists = function(infile) {
+    var instr = infile.toString();
+    if(!fs.existsSync(instr)) {
+        console.log("%s does not exist. Exiting.", instr);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    }
+    return instr;
+};
+
+
+var loadChecks = function(checksfile) {
+    return JSON.parse(fs.readFileSync(checksfile));
+};
+
+var getUrlBuffer = function(url) {
+	var buffer = 'BBBBBBBBBBBBBB';
+	rest.get(url).on('complete', function(result) {
+		  if (result instanceof Error) {
+			console.log('Error: ' + result.message);
+		    this.retry(5000); // try again after 5 sec
+		  } else {
+			  //console.log(result);
+			  checkHtmlFile(result, program.checks);
+		  }
+		});	    
+};
+
+var checkHtmlFile = function(htmlfileBuffer, checksfile) {
+    $ = cheerio.load(htmlfileBuffer);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    var outJson = JSON.stringify(out, null, 4);
+    console.log(outJson);
+
+};
+
+var clone = function(fn) {
+    // Workaround for commander.js issue.
+    // http://stackoverflow.com/a/6772648
+    return fn.bind({});
+};
+
+
+
+if(require.main == module) {
+    program
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <html_url>', 'URL to index.html')
+        .parse(process.argv);
+    var htmlfileBuffer = null;
+    if (program.url) {
+    	htmlfileBuffer = getUrlBuffer(program.url);
+    } else if (program.file) {
+    	htmlfileBuffer = fs.readFileSync(program.file);
+    	checkHtmlFile(htmlfileBuffer, program.checks);
+    } else {
+    	console.log('No file or url was provided');
+    	return;
+    }
+    
+} else {
+    exports.checkHtmlFile = checkHtmlFile;
+}
