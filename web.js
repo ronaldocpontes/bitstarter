c15271f309c0b524e0391df78b66cd987ebf3663
Ronaldo Pontes <ronaldocpontes@yahoo.com.br>
2013-07-07 18:09:01 +0100

    added require fs

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	var index = fs.readFileSync('index.html', {encoding:'utf-8'});
+	var index = fs.readFileSync('index.html', 'utf8');
 	response.send(index);
 });
 
